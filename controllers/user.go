ba6b9d85af9d4d9b7cfc9981356b75662b2b5ab3
Nath <nathanfxs17@gmail.com>
2025-11-24 11:52:47 -0300

    feat: Bycript settup

--- controllers/user.go
+++ controllers/user.go
@@ -1,16 +1,19 @@
 package UserController
 
 import (
-	Api "github.com/Natsukie-7/Nath-chat-ApiRest/utils"
+	Api "github.com/Natsukie-7/Nath-chat-ApiRest/utils/api"
+	"github.com/Natsukie-7/Nath-chat-ApiRest/utils/bycrypt"
 	"github.com/gofiber/fiber/v3"
 )
 
 func GetUser(ctx fiber.Ctx) error {
+	pass, _ := bycrypt.HashPassword("Nath")
+
 	return Api.Resp{
 		Status:  201,
-		Message: "Teste6",
+		Message: "Sucesso",
 		Data: Api.DataStruct{
-			"User": "nath",
+			"User": pass,
 		},
 	}
 }
--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/Natsukie-7/Nath-chat-ApiRest/routes"
-	Api "github.com/Natsukie-7/Nath-chat-ApiRest/utils"
+	Api "github.com/Natsukie-7/Nath-chat-ApiRest/utils/api"
 	"github.com/gofiber/fiber/v3"
 	"github.com/gofiber/fiber/v3/middleware/cors"
 	"github.com/gofiber/fiber/v3/middleware/logger"
@@ -29,6 +31,7 @@ func main() {
 
 	routes.SetupRoutes(app)
 
+	fmt.Println("Aplicação inicializada: http://127.0.0.1:3000")
 	app.Listen(":3000", fiber.ListenConfig{
 		DisableStartupMessage: true,
 	})
